2148f4da0055648dbfb1be81849e01830072ad94
Lazar Stricevic <lucky@uns.ac.rs>
2014-05-13 01:27:35 +0200

    Dodato vremensko ogranicenje za odgovor, ne ispisuje se tacnost odgovora, procenat umesto broja poena

--- zxquiz.c
+++ zxquiz.c
@@ -1,22 +1,24 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 #define BR_PIT 10  // Ukupan broj pitanja u bazi
 #define BR_ODG 4   // Broj odgovora po pitanju
 #define BR_KVPIT 5 // Broj pitanja u kvizu
+#define VREME_PIT 60  //Vreme po pitanju (sek.)
 
 char *pitanja[BR_PIT]={
 	"Koje godine se 'ZX Spectrum' pojavio na trzistu?",
 	"Koji procesor se nalazi u 'ZX Spectrum'-u?",
-	"Odakle ZX imenu 'Speccy'-ja?",
-	"Koliko memorijskih cipova ima 'ZX Spectrum 48K?'",
+	"Odakle 'ZX' imenu 'Spectrum'-a?",
+	"Koliko memorijskih cipova ima 'ZX Spectrum 48K'?",
 	"Kako se zove autor 'Sinclair BASIC'-a?",
 	"Koja je velicina ROM-a koji se nalazi u 'ZX Spectrum 48K'?",
 	"Na kojoj adresi pocinje printer bafer u RAM-u 'ZX Spectrum-a'?",
 	"Koja je graficka rezolucija 'ZX Spectrum'-a?",
 	"Kako se zvala kompanija koja je proizvela 'ZX Spectrum 48k'?",
-	"Koliki je raspon tonova zvucnog izlaza 'ZX Spectrum 48k?'"
+	"Koliki je raspon tonova zvucnog izlaza 'ZX Spectrum 48k'?"
 };
 
 char *odgovor[BR_PIT*BR_ODG]={
@@ -49,13 +51,12 @@ char ipit[BR_PIT];
 
 int pit_br;
 int odg_br;
-int br_poena=0;
 int tipka;
 
 
-// Funkcija koja vraca sadrzaj FRAMES
+// Funkcija vraca sadrzaj FRAMES
 // (sluzi kao slucajan broj)
-int my_rand(){
+int my_rand(void){
 	#asm
 	ld a, (23672)
 	ld l, a
@@ -63,7 +64,7 @@ int my_rand(){
 }
 
 // Inicijalizacija sl. niza indeksa (Fisher–Yates shuffle)
-ipit_init(){
+ipit_init(void){
 	int i,j;
 	for(i=0;i<BR_PIT;i++){
 		j=rand()%(i+1);
@@ -73,12 +74,30 @@ ipit_init(){
 	
 }
 
+// Ocitava tastaturu (LASTK) tokom zadatog vremena
+int get_timedkey(void)
+{
+	int key;
+	long end_time = clock()+(VREME_PIT*50);
+	while(1){
+		if(end_time<= clock()) break;
+		key=getk();
+		if(key>'0' && key<=(BR_ODG+'0')) break;
+		if(key==' ') break;
+	}
+	return key;
+}
 
 main(){
 	int i;
+	int br_poena;
+	int proc_poena;
+
 	printf("%c%c%c**********  ZX  KVIZ  **********\n\n",12,1,32);
-	printf("%c%cDobrodosli u kviz o poznavanju jednog o najpopularnijih 8-bitnih racunara 80-tih godina u bivsoj SFRJ, 'ZX Spectrum'-a. ",1,64);
-	printf("Kroz %d pitanja bice provereno Vase znanje o pojedinostima o njegovom nastanku i funkcionisanju.", BR_KVPIT);
+	printf("%c%cDobrodosli u kviz o poznavanju jednog o najpopularnijih 8-bitnih racunara 80-tih godina, 'ZX Spectrum'-a. ",1,64);
+	printf("Kroz %d pitanja bice provereno Vase znanje o pojedinostima o njegovom nastanku i funkcionisanju. ", BR_KVPIT);
+	printf("Za svako pitanje je ponudjeno %d odgovora, koje birate pritiskom na tastere '1'-'%d'. ", BR_ODG, BR_ODG);
+	printf("Vreme za odgovor na svako pojedinacno pitanje je %d sekundi. ", VREME_PIT);
 	printf("\nZa pocetak kviza pritisnite bilo koju tipku. Srecno!");
 	getchar();
 	srand(my_rand());
@@ -94,28 +113,30 @@ main(){
 		}
 		printf("\nTipke 1-4 za izbor odgovora\nTipka BREAK za izlazak\n");
 
-		while(1){
-			tipka=getchar();
-			if(tipka>'0' && tipka<=(BR_ODG+'0')) break;
-			if(tipka==' ') goto kraj;
-		}
-		printf("Vas odgovor je %c\n\n", tipka);
-		if((tipka-'0')==tacno[pit_br]){
+		tipka=get_timedkey();
+		if(!tipka) continue;
+		if(tipka==' ') break;
+
+//Deo koji ispisuje tacnost odgovora i ceka na tipku za nastavak
+
+//		printf("Vas odgovor je %c\n\n", tipka);
+		if((tipka-'0')==tacno[pit_br])
+//		{
 			br_poena++;
-			printf("TACNO!!!\n\n");
-		} else printf ("Pogresno.\n\n");
-		if(i!=BR_KVPIT-1){
+//			printf("TACNO!!!\n\n");
+//		} else printf ("Pogresno.\n\n");
+/*		if(i!=BR_KVPIT-1){
 			printf("Za nastavak pritisnite bilo koju tipku");
 			getchar();
 		}
-
+*/
 		
 	};
-kraj:
-	printf("Broj osvojenih poena je %d od mogucih %d\n", br_poena, BR_KVPIT);
+	proc_poena=(100*br_poena)/BR_KVPIT;
+	printf("%cProcenat osvojenih poena je %d%%\n\n", 12, proc_poena);
 	printf("Da li zelite da pokusate jos jednom? (D/N)\n");
 	tipka=getchar();
 	if(tipka!='d' && tipka!='D') break;
 	}
-	printf("Hvala na ucescu u kvizu!\n\n");
+	printf("\n\nHvala na ucescu u kvizu!\n");
 }
